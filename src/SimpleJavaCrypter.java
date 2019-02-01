35c13d6743165cc7fa10d21a71967f1d40243fd1
MasterFlomaster1 <30301197+MasterFlomaster1@users.noreply.github.com>
2019-02-01 21:52:17 +0300

    Small changes in main class.

--- src/SimpleJavaCrypter.java
+++ src/SimpleJavaCrypter.java
@@ -3,10 +3,6 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class SimpleJavaCrypter extends JFrame{
-    private JButton btn1 = new JButton("Encrypt");
-    private JButton btn2 = new JButton("Decrypt");
-    private JLabel lbl1 = new JLabel("Enter your line here: ");
-    private JLabel lbl2 = new JLabel("Result: ");
     private JTextField field = new JTextField();
     private JTextField field2 = new JTextField();
 
@@ -15,48 +11,60 @@ public class SimpleJavaCrypter extends JFrame{
     }
 
 
-    public SimpleJavaCrypter(){
+    private SimpleJavaCrypter(){
         super("SimpleJavaCrypter");
+
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         this.setSize(410, 210);
         this.setResizable(false);
-        this.setVisible(true);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
         this.setLayout(null);
 
-        lbl1.setBounds(139, 10, 132, 20);
+        JLabel lbl1 = new JLabel("Enter your line here: ");
+        lbl1.setBounds(155, 10, 132, 20);
         this.add(lbl1);
 
         field.setBounds(50, 40, 310, 20);
         this.add(field);
 
+        JButton btn1 = new JButton("Encrypt");
         btn1.setBounds(50, 70, 150, 30);
         btn1.addActionListener(new ActionListener1());
         this.add(btn1);
 
+        JButton btn2 = new JButton("Decrypt");
         btn2.setBounds(210, 70, 150, 30);
         btn2.addActionListener(new ActionListener2());
         this.add(btn2);
 
+        JLabel lbl2 = new JLabel("Result: ");
         lbl2.setBounds(184, 110, 42, 20);
         this.add(lbl2);
 
         field2.setBounds(50, 140, 310, 20);
         this.add(field2);
 
+        this.setVisible(true);
     }
 
 
     private String crypt(boolean encrypting, String line) {
-        String encrypted = "";
+        StringBuilder encrypted = new StringBuilder();
         for (int i=0;i<line.length();i++) {
-            encrypted += cryptChar(encrypting, line.charAt(i));
+            encrypted.append(cryptChar(encrypting, line.charAt(i)));
         }
-        return encrypted;
+        return encrypted.toString();
     }
 
     private char cryptChar(boolean encrypt, char c) {
-        String CHECKS = ("abcdefghijklmnopqrstuvwxyz1234567890");
-        String REPLACES = ("0qa9zxs3wedc6vf28r5tgb7nhyujm1kiolp4");
+        String CHECKS = ("abcdefghijklmnopqrstuvwxyz1234567890 ,.!?:;()*&^%$#@-=+_");
+        String REPLACES = ("0&q^a9z% x$s3.w)ed:c6v#f2,8*r5tg-b7nhy?uj!m1k(i;olp@+4_");
         int index = 0;
         if (encrypt) {
             for (int i = 0; i < CHECKS.length(); i++) {
