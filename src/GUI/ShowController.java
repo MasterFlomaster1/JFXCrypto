85780f5d3fa833e519479c77876df8174f7e3a51
MasterFlomaster1 <8usernameunknown8@gmail.com>
2019-08-07 05:24:53 +0300

    GUI update
    Update ShowKey appearance.

---
+++ src/GUI/ShowController.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import Cipher.CurrentCipher;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class ShowController {
+
+
+    @FXML
+    private Text cipherName;
+
+    @FXML
+    private TextField keyField;
+
+    public void initialize() {
+        cipherName.setText(CurrentCipher.getCurrentCipherName()+" KEY");
+        keyField.setText(CurrentCipher.getKey());
+    }
+
+}
